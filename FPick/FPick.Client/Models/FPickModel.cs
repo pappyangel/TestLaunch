b3819238147952ccbb3e3c13c8f2d5be5194f475
MSFTJim <jamesvac@live.com>
2024-02-13 13:17:29 -0500

    Add FPickModel and FPicker page

---
+++ FPick/FPick.Client/Models/FPickModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FPick.Client.Models;
+
+public class FPickModel
+{
+    public string? FileName { get; set; }
+    public string? ContentType { get; set; }
+    public byte[]? Data { get; set; }
+
+}
+
+
